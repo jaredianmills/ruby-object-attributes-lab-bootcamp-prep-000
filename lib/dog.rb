d39c76a49ce5022e67bed83474dffa9be0016a53
jaredianmills <jaredianmills@gmail.com>
2018-05-16 21:10:54 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,19 @@
 class Dog
 
-  def name
-    @name
-  end
-
   def name=(dog_name)
     @name = dog_name
   end
 
-  def breed
-    @breed
+  def name
+    @name
   end
 
   def breed=(breed_name)
     @breed = breed_name
+  end
+
+  def breed
+    @breed
   end
 
 end
